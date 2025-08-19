275d647a1c7226ce0c89fed73a42aab4575f47e0
ryoliver <rutholiver@bren.ucsb.edu>
2025-08-19 11:16:51 -0700

    morning warm up

---
+++ wrangling-practice.R
@@ -0,0 +1,33 @@
+# Clearing environment
+rm(list = ls())
+
+# Attach packages
+library(tidyverse)
+library(palmerpenguins)
+library(lubridate) # help us work with dates
+
+# Data wrangling refresher
+# 1. only include penguins at Briscoe and Dream Islands
+# 2. remove the year and sex variables
+# 3. add a new column called body_mass_kg with penguin mass converted from grams to kg
+# 4. rename the island variable to location
+penguins %>%
+  filter(island %in% c("Briscoe", "Dream")) %>%
+  select(-year, -sex) %>%
+  mutate("body_mass_kg" = body_mass_g / 1000) %>%
+  rename(location = island)
+  
+# 1. Limit to only Adelie penguins
+# 2. Remove any observations where flipper_length_mm is NA
+# 3. Group the data by sex
+# 4. Find the mean, standard deviation, and sample size (n()) of flipper lengths for male and females 
+
+penguins %>%
+  filter(species == "Adelie") %>%
+  filter(!is.na(flipper_length_mm),
+         !is.na(sex)) %>% # remove rows that are not NA
+  group_by(sex) %>%
+  summarise(mean = mean(flipper_length_mm),
+            standard_dev = sd(flipper_length_mm),
+            sample_size = n())
+  
